e9e556489aa46ea4e56f911dc72f73aff1bf806b
prernacha <50568242+prernacha@users.noreply.github.com>
2020-06-12 23:16:07 +0530

    Add project files.

---
+++ DatingAPI/Data/DataContext.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using DatingAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace DatingAPI.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options) { }
+
+        public DbSet<Value> Values { get; set; }
+
+    }
+}
