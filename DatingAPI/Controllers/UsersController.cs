f8cbb73d56143069a680bd7b69f61ac4dfa4be99
prerna2112 <prerna.ip.21@gmail.com>
2020-07-07 23:54:44 +0530

    Update user profile modules

--- DatingAPI/Controllers/UsersController.cs
+++ DatingAPI/Controllers/UsersController.cs
@@ -7,15 +7,16 @@ using DatingAPI.Data;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 using DatingAPI.Dtos;
+using System.Security.Claims;
 
 namespace DatingAPI.Controllers
 {   [Authorize]
     [Route("api/[controller]")]
     [ApiController]
-    public class UsersController :ControllerBase
+    public class UsersController : ControllerBase
     {
         private readonly IMapper _impapper;
-        private  readonly IDatingRepository _datingRepository;
+        private readonly IDatingRepository _datingRepository;
         public UsersController(IDatingRepository datingRepository, IMapper imapper)
         {
             _datingRepository = datingRepository;
@@ -27,7 +28,7 @@ namespace DatingAPI.Controllers
         {
             var users = await _datingRepository.GetUsers();
             var usersToReturn = _impapper.Map<IEnumerable<UserForListDto>>(users);
-            return Ok (usersToReturn);
+            return Ok(usersToReturn);
         }
         [HttpGet("{id}")]
         public async Task<IActionResult> GetUser(int id)
@@ -35,6 +36,22 @@ namespace DatingAPI.Controllers
             var user = await _datingRepository.GetUser(id);
             var uerToReturn = _impapper.Map<UerForDetailedDto>(user);
             return Ok(uerToReturn);
+        }
+        [HttpPut("{id}")]
+        public async Task<IActionResult> UpdateUser(int id,UserForUpdate userUpdate)
+        {
+            if(id != int.Parse(User.FindFirst(ClaimTypes.NameIdentifier).Value))
+            {
+                return Unauthorized();
+
+            }
+            var UserFromRepo = await _datingRepository.GetUser(id);
+            _impapper.Map(userUpdate, UserFromRepo);
+            if (await _datingRepository.SaveAll())
+            {
+                return NoContent();
+            }
+            throw new Exception("Failed on update");
         }
     }
 }
--- DatingAPI/Helpers/AutoMApperProfiles.cs
+++ DatingAPI/Helpers/AutoMApperProfiles.cs
@@ -19,6 +19,7 @@ namespace DatingAPI.Helpers
                  .ForMember(dest => dest.PhotoUrl, opt => opt.MapFrom(src => src.Photos.FirstOrDefault(p => p.IsMain).Url))
                  .ForMember(dest => dest.Age, opt => opt.MapFrom(src => src.DateOFBirth.Age()));
             CreateMap<Photo, PhotosForDetailedDto>();
+            CreateMap<UserForUpdate, User>();
 
         }
     }
